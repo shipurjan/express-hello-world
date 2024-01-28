2a00f359e634d57dfd86abcd4a590b28cb8563dd
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-01-28 18:23:06 +0100

    add cors middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors({ origin: true, credentials: true }));
 
 const places = [
   {
